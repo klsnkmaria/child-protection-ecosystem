7f7d69cbac4b16d6d18bf6798bcf6f1cd592b22b
klsnkmaria <168370929+klsnkmaria@users.noreply.github.com>
2026-06-17 04:22:23 +0300

    Create App.tsx

---
+++ frontend-kid/src/App.tsx
@@ -0,0 +1,12 @@
+// frontend-kid/src/App.tsx
+import AvatarChat from './components/AvatarChat';
+
+function App() {
+  return (
+    <div className="App">
+      <AvatarChat />
+    </div>
+  );
+}
+
+export default App;
